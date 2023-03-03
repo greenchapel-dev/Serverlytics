4f12fb847a1758e0432d63f33b172fb262c180b4
greenchapeljohn <greenchapeljohn@gmail.com>
2023-03-03 23:16:53 +0000

    Add post compile task to build the diagrams

--- .projenrc.js
+++ .projenrc.js
@@ -15,11 +15,20 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   ], /* Runtime dependencies of this module. */
   devDeps: [
     '@types/aws-lambda',
+    'cdk-dia',
   ], /* Build dependencies for this module. */
   gitignore: [
     '.env',
+    'diagram.dot',
   ], /* Exclude from git. */
 });
+
+const addDia = project.addTask('dia', {
+  description: 'create a new diagram',
+  exec: 'npx cdk-dia',
+});
+
+project.projectBuild.postCompileTask.spawn(addDia);
+
 project.synth();
 
-project.tasks.addEnvironment('APP_NAME', 'Serverlytics');
